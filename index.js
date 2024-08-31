ae84601b78a268f155a467441075aaf896252976
Biruh <lebiruh@yahoo.com>
2024-08-31 11:59:44 +0300

    updated cors origin to be dynamic on docker

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 8000
 
 
-app.use(cors({origin: "https://info-songs.netlify.app"}));
+app.use(cors({origin: process.env.ALLOWED_ORIGIN}));
 
 // Parse JSON request bodies
 app.use(express.json());
